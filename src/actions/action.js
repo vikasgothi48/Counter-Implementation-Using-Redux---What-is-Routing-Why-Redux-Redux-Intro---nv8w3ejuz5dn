184a8d8986ea7e16bbf71906c7f5740a6dde6a1d
Vikas Gothi <125548865+vikasgothi48@users.noreply.github.com>
2023-07-13 15:21:35 +0530

    Update action.js

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,30 +1,10 @@
-export const addNumber=()=>{
-    return{
-        type: 'INCREMENT'
+export const addNumber = ()=>{
+    return {
+        type:'INCREMENT'
     }
 }
-
-export const subNumber=()=>{
-    return{
-        type: 'DECREMENT'
-    }
-}
-
-export const handlelogin=()=>{
-    return{
-        type: 'SIGN_IN'
-    }
-}
-
-export const handlelogout=()=>{
-    return{
-        type: 'SIGN_OUT'
-    }
-}
-
-export const addAmount=(val)=>{
-    return{
-        type: 'INCREMENTBYAMOUNT',
-        payload: val
+export const subNumber = ()=>{
+    return {
+        type:'DECREMENT'
     }
 }
